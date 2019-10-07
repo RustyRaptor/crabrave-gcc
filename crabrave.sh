be15737fdac0eb2a379894302464e4e969b10461
RustyRaptor <ziada@nmsu.edu>
2019-10-07 13:42:49 -0600

    initial commit

---
+++ crabrave.sh
@@ -0,0 +1,25 @@
+cvlc crabravecut.ogg & echo 🦀🦀🦀🦀🦀
+
+for ((i = 0 ; i < 8 ; i++)); do
+	echo 🦀
+	sleep 1
+done
+
+for ((i = 0 ; i < 7 ; i++)); do
+	for ((j = 0 ; j < i*2 ; j++)); do
+        	printf "🦀"
+	done
+sleep 1 
+done
+
+printf "\n"
+
+for ((i = 7 ; i < 14 ; i++)); do
+        for ((j = 7 ; j < i*2 ; j++)); do
+                printf "🦀"
+        done
+echo IT COMPILED
+sleep 1
+done
+
+killall vlc
